943d0af0c0ecde0e1f6b4ea69e489e9ad96d004b
philipkariuki <kariukiphil@gmail.com>
2019-06-21 14:29:20 +0300

    Upto add css- shows movie thumbnails

--- app/views.py
+++ app/views.py
@@ -1,15 +1,15 @@
-from flask import render_template
-from app import app
+# from flask import render_template
+# from app import app
 
-@app.route('/')
-def index():
+# @app.route('/')
+# def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+#     title = 'Home - Welcome to The best Movie Review Website Online'
+#     return render_template('index.html', title = title)
 
 
 
@@ -35,3 +35,54 @@ def index():
 
 #     message = 'Hello World'
 #     return render_template('index.html',message = message)
+
+
+# from flask import render_template
+# from app import app
+# from .request import get_movies
+
+# @app.route('/')
+# def index():
+
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+
+#     # Getting popular movie
+#     popular_movies = get_movies('popular')
+#     upcoming_movie = get_movies('upcoming')
+#     now_showing_movie = get_movies('now_playing')
+#     title = 'Home - Welcome to The best Movie Review Website Online'
+#     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+
+from flask import render_template
+from app import app
+from .requests import get_movies,get_movie
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+    title = 'Home - Welcome to The best Movie Review Website Online'
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
