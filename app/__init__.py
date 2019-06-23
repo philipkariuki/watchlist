34f874466b36644c3427457e82ad6950b5fe9831
philipkariuki <kariukiphil@gmail.com>
2019-06-23 17:05:54 +0300

    Up to restructured application

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,50 @@
+# from flask import Flask
+# from flask_bootstrap import Bootstrap
+# from .config import DevConfig
+
+# # Initializing application
+# app = Flask(__name__,instance_relative_config = True)
+
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+
+# # Initializing Flask Extensions
+# bootstrap = Bootstrap(app)
+
+
+# from app import views
+# from app import error
+
+
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .config import DevConfig
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    app = Flask(__name__)
+
+    # Creating the app configurations
+
+    app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
 
-from app import views
-from app import error
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+
+
+
+    return app
--- app/main/views.py
+++ app/main/views.py
@@ -1,73 +1,14 @@
-# from flask import render_template
-# from app import app
-
-# @app.route('/')
-# def index():
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-
-#     title = 'Home - Welcome to The best Movie Review Website Online'
-#     return render_template('index.html', title = title)
-
-
-
-
-# @app.route('/movie/<int:movie_id>')
-# def movie(movie_id):
-
-#     '''
-#     View movie page function that returns the movie details page and its data
-#     '''
-#     return render_template('movie.html',id = movie_id)
-
-
-
-
-#     # Views
-# @app.route('/')
-# def index():
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-
-#     message = 'Hello World'
-#     return render_template('index.html',message = message)
-
-
-# from flask import render_template
-# from app import app
-# from .request import get_movies
-
-# @app.route('/')
-# def index():
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-
-#     # Getting popular movie
-#     popular_movies = get_movies('popular')
-#     upcoming_movie = get_movies('upcoming')
-#     now_showing_movie = get_movies('now_playing')
-#     title = 'Home - Welcome to The best Movie Review Website Online'
-#     return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
-
 
 from flask import render_template,request,redirect,url_for
-from app import app
-from .requests import get_movies,get_movie,search_movie
-from .models import review
+from . import main
+from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm
+from ..models import Review
 
 
 
-Review = review.Review
 
-
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -89,7 +30,7 @@ def index():
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -104,7 +45,7 @@ def movie(id):
 
 
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -117,7 +58,7 @@ def search(movie_name):
 
 
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
