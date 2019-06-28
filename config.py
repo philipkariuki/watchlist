1dff2958ce5119ce67cb9bdd600d0f527fed9c91
philipkariuki <kariukiphil@gmail.com>
2019-06-29 01:01:21 +0300

    working upload profile picture

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://phirifo:1234@localhost/watchlist'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 
--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
-from app import create_app
-from flask_script import Manager,Shell,Server
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User,Role
+from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
 
 manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
@@ -14,6 +18,11 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(testsg)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Role = Role )
+
 
 if __name__ == '__main__':
     manager.run()
